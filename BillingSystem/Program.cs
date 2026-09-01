7f57d9d2f5c4afbcf6f1c7dda0ea11a8db631dbf
johnmarkestillerdavalos04-dot <johnmarkestiller.davalos25@bicol-u.edu.ph>
2026-09-01 22:39:06 +0800

    Update Program.cs
    oopss oops oops

--- BillingSystem/Program.cs
+++ BillingSystem/Program.cs
@@ -11,7 +11,7 @@ namespace BillingSystem
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new LoginForm()); // Edited by Davalos;
+            Application.Run(new LoginForm(YOOHOOO DITO ANG TINGIN)); // Edited by Davalos;
         }
     }
 }
