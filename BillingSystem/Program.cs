3c01823dd962fd3ab0d73818a9818512a8379716
Vinz-Parillas <vinzxandenamaro.parillas25@bicol-u.edu.ph>
2026-09-02 13:35:21 +0800

    Change main form from CustomerList to AddCustomer
    conflict

--- BillingSystem/Program.cs
+++ BillingSystem/Program.cs
@@ -11,7 +11,7 @@ namespace BillingSystem
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new CustomerList()); // new conflict
+            Application.Run(new AddCustomer()); // new conflict
         }
     }
 }
